490fe909becd94aedc60ec5d9855bca7309c3373
shimokp <oxo_inflove@yahoo.co.jp>
2016-06-14 16:26:37 +0900

    Initial Commit

---
+++ VASILY-Intern2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VASILY-Intern2016
+//
+//  Created by 下村一将 on 2016/06/14.
+//  Copyright © 2016年 kazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
